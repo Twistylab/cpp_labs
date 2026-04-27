c17cf4339b1fb840b1060c3944d607d99fff11e0
Nikita Zotov <nikita.zotov@metalab.ifmo.ru>
2026-04-27 08:53:52 +0300

    add script dev -> stg and stg -> prd

---
+++ script_dev_to_stg.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+git checkout dev
+git pull origin dev
+
+git checkout stg
+git pull origin stg
+
+git merge dev -m "Auto merge dev to stg"
+
+TAG="stg-$(date +%Y-%m-%d-%H%M%S)"
+git tag -a "$TAG" -m "Moved to stg $TAG"
+
+git push origin stg
+git push origin "$TAG"
+
+echo "Moved dev -> stg with tag $TAG"
